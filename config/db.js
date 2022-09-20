45237a7fb93c968e17f419c5f7ae4651a87a724a
alex <alexcarvajal2404@gmail.com>
2022-09-20 11:07:54 -0500

    initial commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,16 @@ const { Sequelize } = require('sequelize');
 
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('db_alishop', 'alishop', '1234', {
-  host: 'localhost',
+// const sequelize = new Sequelize('db_alishop', 'alishop', '1234', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 43306,
+// });
+
+const sequelize = new Sequelize('railway', 'root', 'kj5WXZqm7PqKaMmp0jm3', {
+  host: 'containers-us-west-52.railway.app',
   dialect: 'mysql',
-  port: 43306,
+  port: 7939,
 });
 
 
